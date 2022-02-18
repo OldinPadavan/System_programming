2a93f715835e01ba1e6528f6113baccab8726bc3
OldinPadawan <diyandmakers@gmail.com>
2022-02-18 21:56:20 +0300

    add full stack of classes and variables of classes

--- Objects/Objects.cpp
+++ Objects/Objects.cpp
@@ -2,25 +2,78 @@
 //
 
 #include <iostream>
+#include <string>
+using std::string;
+using std::cout;
+using std::cin;
+using std::endl;
 
 class truck {
+    double weight;
+    int max_transprted_weight;
+    string vendor;
+    string model_name;
+    string registration_number;
+    
+public :
+    truck(double weight, int max_transprted_weight, string vendor, string model_name, string registration_number) {
+        this->weight = weight;
+        this->max_transprted_weight = max_transprted_weight;
+        this->vendor = vendor;
+        this->model_name = model_name;
+        this->registration_number = registration_number;
+    }
+    truck() {
+        weight = 2000.0;
+        max_transprted_weight = 5000.0;
+        vendor = "Kamaz";
+        model_name = "5490-DC NEO";
+        registration_number = "Y500ZT78";
+    }
 
+    double getWeight() {
+        return weight;
+    }
+
+    double getMax_moving_weight() {
+        return max_transprted_weight;
+    }
+    
+    void ShowRegistrationNumber() {
+        cout << registration_number << endl;
+    }
 };
 
 class excavator {
-
+    double weight;
+    double max_lift_weight;
+    int driver_seat_count;
+    string vendor;
+    string model_name;
 };
 
 class crane {
-
+    double max_lift_weight;
+    double height;
+    double weight;
+    string vendor;
+    string model_name;
 };
 
 class paver /*асфальтоукладчик*/ {
-
+    double weight;
+    double transported_weight;
+    double storage_capacity;
+    string vendor;
+    string model_name;
 };
 
 class tractor {
-
+    double weight;
+    double max_speed;
+    int driver_seat_count;
+    string vendor;
+    string origin_made_country;
 };
 
 int main()
